6488db016d8dcb6221167ead2899f907873f16aa
t2haev00 <eveliinahampus@gmail.com>
2023-03-29 19:50:21 +0300

    backend to git

---
+++ index.js
@@ -0,0 +1,66 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// Description: This is the main entry point for the server
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const pg_1 = require("pg");
+// Create a new express application instance
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
+// The port the express app will listen on
+const port = 3001;
+/*
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({result: 'csuccess'})
+})
+*/
+// Get all tasks
+app.get('/', (req, res) => {
+    const pool = openDb();
+    pool.query('select * from task', (error, result) => {
+        if (error) {
+            res.status(500).json({ error: error.message });
+        }
+        res.status(200).json(result.rows);
+    });
+});
+// Open a connection to the database
+const openDb = () => {
+    const pool = new pg_1.Pool({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'todo',
+        password: 'Mickey37#',
+        port: 5432
+    });
+    // Return the connection pool
+    return pool;
+};
+// Post a new task
+app.post('/new', (req, res) => {
+    const pool = openDb();
+    pool.query('insert into task (description) values ($1) returning *', [req.body.description], (error, result) => {
+        if (error) {
+            res.status(500).json({ error: error.message });
+        }
+        res.status(200).json({ id: result.rows[0].id });
+    });
+});
+//  Delete a task
+app.delete('/delete/', (req, res) => {
+    const pool = openDb();
+    const id = parseInt(req.params.id);
+    pool.query('delete from task where id = $1', [id], (error, result) => {
+        if (error) {
+            res.status(500).json({ error: error.message });
+        }
+        res.status(200).json({ id: id });
+    });
+});
+// Listen on the port
+app.listen(port);
---
+++ index.ts
@@ -0,0 +1,87 @@
+// Description: This is the main entry point for the server
+import express, { Express, Request, Response } from 'express'
+import cors from 'cors'
+import { Pool, QueryResult } from 'pg'
+
+// Create a new express application instance
+const app: Express = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false}))
+
+// The port the express app will listen on
+
+const port = 3001
+
+/*
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({result: 'csuccess'})
+})
+*/
+
+// Get all tasks
+
+app.get ('/', (req: Request, res: Response) => {
+    const pool = openDb()
+
+    pool.query('select * from task', (error, result) => {
+        if (error) {
+            res.status(500).json({error: error.message})
+        } 
+            res.status(200).json(result.rows)
+        })
+    })
+
+// Open a connection to the database
+    
+const openDb = (): Pool => {
+    const pool = new Pool ({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'todo',
+        password: 'Mickey37#',
+        port: 5432
+    })
+
+// Return the connection pool
+    return pool
+}
+
+
+
+// Post a new task
+
+app.post('/new', (req: Request, res: Response) => {
+        const pool = openDb()
+
+        pool.query('insert into task (description) values ($1) returning *',
+        [req.body.description], 
+        (error: Error, result: QueryResult) => {
+            if (error) {
+                res.status(500).json({error: error.message})
+            }
+            res.status(200).json({id: result.rows[0].id})
+        })
+    }) 
+
+
+//  Delete a task
+app.delete('/delete/', (req: Request, res: Response) => {
+    const pool = openDb()
+
+    const id = parseInt(req.params.id)
+
+    pool.query('delete from task where id = $1', 
+    [id], 
+    (error: Error, result: QueryResult) => {
+        if (error) {
+            res.status(500).json({error: error.message})
+        }
+
+        res.status(200).json({id: id})
+    })
+})
+
+
+// Listen on the port
+app.listen(port)    
